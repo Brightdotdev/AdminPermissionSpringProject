6c2dbcef4617fccc3ed8e240e32e25cce494e7f9
Bright <iamtherealbright@gmail.com>
2025-01-28 12:10:10 +0000

    Inital commit

---
+++ AdminPermission/src/main/java/com/example/AdminPermission/AdminPermissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.AdminPermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPermissionApplication.class, args);
+	}
+
+}
---
+++ AdminPermission/src/test/java/com/example/AdminPermission/AdminPermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdminPermission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPermissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
